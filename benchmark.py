5f330e405b2259552992eca02da671673cb31aa4
Neumann, Jan <jan.neumann@oracle.com>
2024-01-25 16:02:10 +0100

    Add argparse dataset and devices selection to benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,21 +1,39 @@
+import argparse
+from typing import Literal
+from typing import get_args as t_get_args
+
 from lib.benchmarks.runnables.neuralogic_cpu_runnable import NeuraLogicCPURunnable
+from lib.benchmarks.runnables.runnable import Runnable
 from lib.benchmarks.runnables.torch_gather_runnable import TorchGatherRunnable
 from lib.benchmarks.runner import MultiRunner
 from lib.datasets.mutagenesis import MyMutagenesis, MyMutagenesisMultip
 from tqdm.auto import tqdm
 
+Device = Literal['mps', 'cuda', 'cpu', 'neuralogic']
+
+
 if __name__ == "__main__":
-    runnables = {
-        'torch_gather_mps': TorchGatherRunnable(device='mps'),
-        'torch_gather_cpu': TorchGatherRunnable(device='cpu'),
-        'neuralogic_cpu': NeuraLogicCPURunnable(),
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--multip', '-m', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('--devices', '-d', choices=t_get_args(Device), nargs='+', required=True)
+    args = parser.parse_args()
+
+    runnables: dict[Device, Runnable] = {
+        "mps": TorchGatherRunnable(device="mps"),
+        "cpu": TorchGatherRunnable(device="cpu"),
+        "cuda": TorchGatherRunnable(device="cuda"),
+        "neuralogic": NeuraLogicCPURunnable(),
     }
 
     runner = MultiRunner(n_repeats=10)
 
-    dataset = MyMutagenesisMultip().build()
+    if args.multip:
+        dataset = MyMutagenesisMultip().build()
+    else:
+        dataset = MyMutagenesis().build()
 
-    for runnable_name, runnable in tqdm(runnables.items(), desc="Runners"):
+    for runnable_name in tqdm(args.devices, desc="Runners"):
+        runnable = runnables[runnable_name]
         runner.measure(runnable_name, runnable, dataset)
 
     print(runner.get_result())
