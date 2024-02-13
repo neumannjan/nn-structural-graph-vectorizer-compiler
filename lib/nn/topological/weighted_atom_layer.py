286a1ab30f743aeb49ef42196e3b204e3c2256fd
Neumann, Jan <jan.neumann@oracle.com>
2024-02-13 19:05:14 +0100

    Change atom layer to gather ops from Python indexing

--- lib/nn/topological/weighted_atom_layer.py
+++ lib/nn/topological/weighted_atom_layer.py
@@ -1,7 +1,22 @@
+import itertools
+from typing import Iterable
+
 import torch
-from lib.nn.gather import build_optimal_gather_module
-from lib.nn.linear import Linear
+from lib.nn.gather import build_optimal_gather_module, build_optimal_single_layer_gather_module_unwrapped
 from lib.nn.topological.layers import Ordinals
+from lib.nn.weight import UnitWeight, Weight, concatenate_weights, create_weight
+from lib.utils import value_to_tensor
+
+
+def _build_weights(weights: Iterable, out_map: dict[str, int], weights_out: list[torch.Tensor]):
+    for weight in weights:
+        w_idx = str(weight.index)
+
+        if w_idx not in out_map:
+            out_map[w_idx] = len(out_map)
+
+            w_tensor = value_to_tensor(weight.value)
+            weights_out.append(w_tensor)
 
 
 class WeightedAtomLayer(torch.nn.Module):
@@ -18,11 +33,45 @@ class WeightedAtomLayer(torch.nn.Module):
             [neuron_ordinals[int(inp.getIndex())] for n in layer_neurons for inp in n.getInputs()],
         )
 
-        self.linear = Linear(layer_neurons, assume_all_weights_same=False)
+        def _iter_all_weights():
+            return (w for n in layer_neurons for w in n.getWeights())
+
+        idx_map: dict[str, int] = {}
+
+        weights_learnable: list[torch.Tensor] = []
+        weights_nonlearnable: list[torch.Tensor] = []
+
+        _build_weights(filter(lambda w: w.isLearnable(), _iter_all_weights()), idx_map, weights_learnable)
+        _build_weights(filter(lambda w: not w.isLearnable(), _iter_all_weights()), idx_map, weights_nonlearnable)
+
+        if len(weights_learnable) > 0:
+            # ensure all learnable weights have the same dimensions
+            assert all(
+                (weights_learnable[0].shape == w.shape for w in weights_learnable[1:])
+            ), f"Learnable weights' dimensions do not match: {[w.shape for w in weights_learnable]}"
+
+            # expand all non-learnable weights to the same shape as learnable weights
+            weights_nonlearnable = [
+                create_weight(t, is_learnable=False).expand_to(weights_learnable[0]) for t in weights_nonlearnable
+            ]
+
+            weight_learnable = torch.nn.Parameter(torch.stack(weights_learnable), requires_grad=True)
+        else:
+            weight_learnable = None
+
+        if len(weights_nonlearnable) > 0:
+            weight_nonlearnable = torch.nn.Parameter(torch.stack(weights_nonlearnable), requires_grad=False)
+        else:
+            weight_nonlearnable = None
+
+        self.weight = concatenate_weights(weight_learnable, weight_nonlearnable)
+
+        weight_idxs: list[int] = [idx_map[str(w.index)] for w in _iter_all_weights()]
+        self.gather_weights = build_optimal_single_layer_gather_module_unwrapped(ordinals=weight_idxs)
 
     def forward(self, layer_values: dict[int, torch.Tensor]):
         input_values = self.gather(layer_values)
-        # TODO reshape ?
-        y = self.linear(input_values)
+        w = self.gather_weights(self.weight())
+        y = w @ input_values
         y = torch.tanh(y)
         return y
--- lib/nn/weight.py
+++ lib/nn/weight.py
@@ -1,10 +1,13 @@
-from typing import Protocol
+from typing import Protocol, Sequence
 
 import torch
 
 
 class WeightLike(Protocol):
     def apply_to(self, to: torch.Tensor) -> torch.Tensor:
+        ...
+
+    def expand_to(self, other: torch.Tensor | torch.Size | tuple[int, ...]) -> torch.Tensor:
         ...
 
     @property
@@ -24,14 +27,28 @@ class Weight(torch.nn.Module, WeightLike):
     def value(self) -> torch.nn.Parameter:
         return self.weight
 
+    def expand_to(self, other: torch.Tensor | torch.Size | tuple[int, ...]) -> torch.Tensor:
+        raise ValueError("Not supported for weights other than UnitWeight")
+
     def extra_repr(self) -> str:
         shape = ",".join(map(str, self.weight.shape))
         return f"{shape}"
+
+    def forward(self) -> torch.Tensor:
+        return self.weight
 
 
 class UnitWeight(Weight, WeightLike):
     def __init__(self) -> None:
         super().__init__(torch.nn.Parameter(torch.tensor([1.0]), requires_grad=False))
+
+    def expand_to(self, other: torch.Tensor | torch.Size | tuple[int, ...]) -> torch.Tensor:
+        shape = other.shape if isinstance(other, torch.Tensor) else other
+
+        if len(shape) == 2 and shape[0] == shape[1]:
+            return torch.eye(shape[0])
+
+        return torch.ones(shape)
 
     def apply_to(self, to: torch.Tensor) -> torch.Tensor:
         return to
@@ -42,3 +59,23 @@ def create_weight(tensor: torch.Tensor, is_learnable: bool) -> Weight:
         return UnitWeight()
 
     return Weight(torch.nn.Parameter(torch.atleast_1d(tensor), requires_grad=is_learnable))
+
+
+class WeightsConcatenation(torch.nn.Module):
+    def __init__(self, weights: Sequence[torch.nn.Parameter]) -> None:
+        super().__init__()
+
+        self.weights = weights
+
+    def forward(self) -> torch.Tensor:
+        return torch.concatenate(self.weights)
+
+
+def concatenate_weights(*weights: torch.nn.Parameter | None) -> torch.nn.Module:
+    weights_nonnull = [w for w in weights if w is not None]
+    if len(weights_nonnull) == 0:
+        raise ValueError()
+    elif len(weights_nonnull) == 1:
+        return Weight(weights_nonnull[0])
+
+    return WeightsConcatenation(weights_nonnull)
